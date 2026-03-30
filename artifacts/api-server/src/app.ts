23a17ea2aeeedbb410dfcae272a9457733e408f1
wanderingboxer <aditya.saxena4052@gmail.com>
2026-03-30 19:32:28 +0530

    Fix pino-http import for Vercel build

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import type { IncomingMessage, ServerResponse } from "http";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
